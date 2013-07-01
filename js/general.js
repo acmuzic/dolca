3a6bad9312ced7c6576990453232cf2cac3b52e3
acmuzic <acmuzic@MacBook-Air.local>
2013-07-01 04:40:03 -0300

    20130701 0439
    .

--- js/general.js
+++ js/general.js
@@ -83,3 +83,10 @@ function sortearPremio()
 		return 0;
 	}
 }
+
+function getPhoneGapPath() {
+    var path = window.location.pathname;
+    path = path.substr( path, path.length - 10 );
+    //return 'file://' + path;
+    return path;
+};
