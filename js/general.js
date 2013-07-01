9c3c4ed99a633b73dfa6fb71f2a85d6fd8fdd214
acmuzic <acmuzic@MacBook-Air.local>
2013-07-01 17:46:04 -0300

    20130701 1745
    .

--- js/general.js
+++ js/general.js
@@ -90,3 +90,35 @@ function getPhoneGapPath() {
     //return 'file://' + path;
     return path;
 };
+
+/*-------------------------------  AUDIO ---------------------------------*/
+var my_media = null;
+var mediaTimer = null;
+function playAudio(src) {
+	if (my_media == null) {
+		// Create Media object from src
+		my_media = new Media(src, onSuccess, onError);
+	} // else play current audio
+	// Play audio
+	my_media.play();
+
+	// Update my_media position every second
+	if (mediaTimer == null) {
+		mediaTimer = setInterval(function() {
+			// get my_media position
+			my_media.getCurrentPosition(
+				// success callback
+				function(position) {
+					if (position > -1) {
+						setAudioPosition((position) + " sec");
+					}
+				},
+				// error callback
+				function(e) {
+					console.log("Error getting pos=" + e);
+					setAudioPosition("Error: " + e);
+				}
+			);
+		}, 1000);
+	}
+}
