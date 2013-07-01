076e6aa049a75f4bf37eecaf3675c432a30673e9
acmuzic <acmuzic@MacBook-Air.local>
2013-07-01 03:52:37 -0300

    20130701 0352
    .

--- js/general.js
+++ js/general.js
@@ -1,9 +1,8 @@
 // JavaScript Document
 
 //Configuración de la app
-var debug = 1;
 var premios_maximo = 25;
-var porcentaje_ganador = 50;
+var porcentaje_ganador = 90;
 
 //Premios
 
@@ -25,61 +24,62 @@ var ganador = window.localStorage.getItem('ganador');
 //Busco la variable cachorro elegido
 var cachorro = window.localStorage.getItem('cachorro');
 
+//Busco la variable penal elegido
+var penal = window.localStorage.getItem('penal');
+	
+
 function chequearPremios() {
+	
+	//alert('premios');
+	
 	//Muestro la fecha
-	alerta('fecha: '+fecha);
+	alert('fecha: '+fecha);
 	
 	//Muestro el estado de la variable ganador al comenzar
-	alerta('ganador onload: '+ganador);
+	//alert('ganador al cargar: '+window.localStorage.getItem('ganador'));
 
 	
 	//Blanqueo el estado de la variable ganador
 	window.localStorage.setItem('ganador', 0);
-	alerta('ganador post chequearPremios(): '+window.localStorage.getItem('ganador'));
+	//alert('ganador blanqueado: '+window.localStorage.getItem('ganador'));
 	
 	//Chequeo si no está creado el contador de premios del dia, sino lo creo
 	if(window.localStorage.getItem(fecha) == null)
 	{
+		alert('creo el contador para fecha:'+fecha);
 		window.localStorage.setItem(fecha, 0);
 	}
-	alerta('premios regalados: '+window.localStorage.getItem(fecha));
 	
 	//Traigo la cantidad de premios que se van regalando hoy
 	var premios = window.localStorage.getItem(fecha);
+	alert('premios regalados a fecha '+fecha+': '+window.localStorage.getItem(fecha));
 	
 	//Si ya entregue la cantidad de premios definida, no entrego mas
+	alert(premios+'/'+premios_maximo);
 	if(premios<premios_maximo)
 	{
-		var gana = sortearPremio();
+		ganador = sortearPremio();
 	}else{
-		var gana = 0;
+		ganador = 0;
 	}
-	alerta('gana: '+gana);
-	
+	alert('ganador?: '+ganador);
+	window.localStorage.setItem('ganador',ganador);
 	//Almaceno para la fecha la cantidad (1 o 0) de premios entregados hoy
-	window.localStorage.setItem(fecha, parseInt(premios)+parseInt(gana));
+	window.localStorage.setItem(fecha, parseInt(premios)+parseInt(ganador));
 	premios = window.localStorage.getItem(fecha);
-	alerta('premios: '+premios);
+	alert('premios almacenados: '+premios);
 }
 
 function sortearPremio()
 {
- var rand = Math.floor((Math.random()*100)+1);
-	 if(rand<porcentaje_ganador)
-	 {
-		 return 0;
-	 }
-	 else
-	 {
-		 return 1;
-	 }
-}
-
-//Modo Debug
-function alerta(texto)
-{
-	if(debug==1)
+	var rand = Math.floor((Math.random()*100)+1);
+	alert('rand:'+rand+' / '+porcentaje_ganador);
+	if(rand<porcentaje_ganador)
 	{
-		alert(texto);
+		return 1;
+	}
+	else
+	{
+		return 0;
 	}
 }
